6ecced6c39e8b59829812f8ede6e86c8f189f857
Kevin Figueroa <89550258+Kevinals75@users.noreply.github.com>
2021-12-09 19:09:14 -0800

    Add files via upload

---
+++ polinomios.s
@@ -0,0 +1,89 @@
+@@ Instituto Tecnologico de Tijuana
+@@ Depto de Sistemas y Computacion
+@@ Ing. En Sistemas Computacionales
+@@ Autor: Kevin Enrique Figueroa Mazo @nickname: Kevinals75
+@@ Fecha de revision: 9/12/21                                           
+
+.data
+var1 :	.asciz " %d \n"
+
+.text
+.global main
+
+/* Salvo registros */
+
+main : push { r4, lr }
+
+/* Introduzco los 4 primeros parametros vía registros */
+
+	mov r0, # 1
+	mov r1, # 2
+	mov r2, # 3
+	mov r3, # 4
+
+/* Introduzco el 5o parámetro por pila */
+
+	mov r4, # 5
+	push { r4 }
+
+/* Llamada a función poly3 (1, 2, 3, 4, 5) */
+	bl poly3
+
+/* Equilibro la pila ( debido al 5o par á metro ) */
+
+	add sp, # 4
+
+/* Paso resultado de la funci ón a r1, cadena a
+	imprimir a r0 y llamo a la funci ón */
+
+	mov r1, r0							@Transfiere datos entre celdas de memoria
+	ldr r0, = var1
+	bl printf
+
+/* Segunda llamada, esta vez poly3 (1, -1, 1, -1, 8) */
+
+	mov r0, # 1						        @Transfiere datos entre celdas de memoria
+	mov r1, # -1							@Transfiere datos entre celdas de memoria
+	mov r2, # 1							@Transfiere datos entre celdas de memoria
+	mov r3, # -1							@Transfiere datos entre celdas de memoria
+	mov r4, # 8							@Transfiere datos entre celdas de memoria
+	push { r4 }
+	bl poly3
+	add sp, # 4
+
+/* Imprimo resultado de segunda llamada */
+
+	mov r1, r0
+	ldr r0, = var1
+	bl printf
+
+/* Llamo e imprimo poly3 (2, 0, 0, 0, 8) */
+
+	mov r0, # 2							@Transfiere datos entre celdas de memoria
+	mov r1, # 0                                                     @Transfiere datos entre celdas de memoria
+	mov r2, # 0							@Transfiere datos entre celdas de memoria
+	mov r3, # 0							@Transfiere datos entre celdas de memoria
+	mov r4, # 8							@Transfiere datos entre celdas de memoria
+	push { r4 }
+	bl poly3
+	add sp, # 4
+	mov r1, r0
+	ldr r0, = var1
+	bl printf
+
+/* Recupero registros y salgo de main */
+
+	pop { r4, lr }
+	bx lr
+
+	.equ param5, 4 *1 /* r4 */
+
+poly3 : push { r4 }							 @ salvaguarda r4
+	ldr r4, [ sp, # param5 ] 					 @ leo r4 de pila
+	smlabb r3, r2, r4, r3 						 @ r3= c*x + d
+	smulbb r2, r4, r4 						 @ r2= x*x
+	smlabb r3, r1, r2, r3 						 @ r3= b*(x*x) + (c*x + d)
+	smulbb r2, r2, r4 						 @ r2= x*(x*x)
+	smlabb r0, r0, r2, r3 						 @ r0= a*x*x*x + b*x*x + c*x+d
+	pop { r4 } 							 @ recupero r4
+	bx lr 							     	 @ salgo de la función
